fad708fd32cb14c99acbe0c47517f6457b0ba952
esakat <esaka.tom@gmail.com>
2020-08-28 05:45:05 +0900

    logger rate

--- employee-importance.py
+++ employee-importance.py
@@ -12,18 +12,29 @@ class Employee:
 
 
 class Solution:
+
+    # BFS Solution
+    # def getImportance(self, employees: List['Employee'], id: int) -> int:
+    #     mp = dict()
+    #     for e in employees:
+    #         mp[e.id] = {"importance": e.importance, "subordinates": e.subordinates}
+    #
+    #     que = Queue()
+    #     que.put(id)
+    #     importance = 0
+    #     while not que.empty():
+    #         now = que.get()
+    #         importance += mp[now]["importance"]
+    #         for e in mp[now]["subordinates"]:
+    #             que.put(e)
+    #
+    #     return importance
+
+    # DFS Solution
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        mp = dict()
-        for e in employees:
-            mp[e.id] = {"importance": e.importance, "subordinates": e.subordinates}
-
-        que = Queue()
-        que.put(id)
-        importance = 0
-        while not que.empty():
-            now = que.get()
-            importance += mp[now]["importance"]
-            for e in mp[now]["subordinates"]:
-                que.put(e)
-
-        return importance
+        edic = {e.id: e for e in employees}
+        def dfs(eid):
+            employee = edic[eid]
+            return (employee.importance +
+                    sum(dfs(eid) for eid in employee.subordinates))
+        return dfs(id)
